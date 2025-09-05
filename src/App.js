b54f6a2b8c6ccbf602bb22a972a3d23e7055fe28
Samarth Magdum <101000624+HackerSM9@users.noreply.github.com>
2025-09-05 12:00:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Footer from "./components/Footer";
-
-import "./App.css"; // global styles for App
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        {/* Navigation Bar */}
-        <Navbar />
-
-        {/* Page Routing */}
+      <div className="app-container">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-
-        {/* Footer */}
-        <Footer />
       </div>
     </Router>
   );
